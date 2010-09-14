7f2867665671b360e43098442406b0506c446185
rlbrackett3 <robert.brackett3@gmail.com>
2010-09-14 14:39:41 -0400

    initial push simple_blog skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBlog::Application.config.secret_token = '508d3ed11a6dea889081e7a8907e71588a5f04ee914eff45e73ad5abcf93e86a79ac1c0e238a5ef3406b4ff36a4f66a98d6babdab0723fcadb0fa52e9566d4c7'
